d9cc5cb2583a8a6bab0cf4a7ca845ad17b098491
anjeelaali <anjeelaali6@gmail.con>
2024-06-15 13:40:32 +0500

    Add header

--- src/app/home/ui/Automated.tsx
+++ src/app/home/ui/Automated.tsx
@@ -1,11 +1,39 @@
+import { Typography, Container } from '@mui/material'
 import React from 'react'
 
 const Automated = () => {
-  return (
-    <>
-      this is automated message 
-    </>
-  )
+    return (
+        <>
+            <Container sx={{
+                textAlign: 'center',
+                border: '1px  solid white',
+                marginTop: '100px',
+                marginBottom: '10px'
+            }}>
+                <Typography variant='h6' sx={{
+                    fontWeight:'500',
+                    letterSpacing:'5px',
+                    fontSize:'14px',
+                    color:'gray',
+                    lineHeight:'47px'
+                }}>
+                    INDUSTRIES
+                </Typography>
+                <Typography variant='h3' sx={{ fontFamily: 'Basement Grotesque', fontSize:'62px' }}>
+            Automated messaging <br />
+            for
+            <Typography
+                component='span'
+                variant='body'
+                sx={{ color: 'text.secondary' }}
+            >
+                {' '}
+                any industry.
+            </Typography>
+        </Typography>
+            </Container>
+        </>
+    )
 }
 
 export default Automated
